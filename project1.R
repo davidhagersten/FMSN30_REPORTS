38d9af817eb5c77490df07697fe896a6b0e42fd7
Gillsbro <erik_gillsbro@hotmail.com>
2016-03-23 14:29:46 +0100

    Update project1.R

--- project1.R
+++ project1.R
@@ -1,4 +1,8 @@
 #Read data from homepage
 data_1 <- read.table("http://www.maths.lth.se/matstat/kurser/masm22/exercise/dataCDI.txt",header=FALSE,sep="")
 colnames(data_1) <- c("id","county","state","area","pop","pop1834","pop65","phys","beds","crimes","higrads","bachelors","poors","unemployed","pci","totinc","region")
+attach(data_1)
+#Plot crimes
+CRM1000 <- (crimes/pop)*1000
+plot(id,CRM1000)
 
