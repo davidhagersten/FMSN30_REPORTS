97cc775a33f735e1d11914bdf9e221acdc7c6d75
davidhagersten <davidhagersten@gmail.com>
2016-03-23 14:08:55 +0100

    Update project1.R

--- project1.R
+++ project1.R
@@ -3,3 +3,5 @@ data <- scan("")
 data_1 <- read.table("http://www.maths.lth.se/matstat/kurser/masm22/exercise/dataCDI.txt",header=FALSE,sep="")
 colnames(data_1) <- c("id","county","state","area","pop","pop1834","pop65","phys","beds","crimes","higrads","bachelors","poors","unemployed","pci","totinc","region")
 data_1
+
+HEJ!
