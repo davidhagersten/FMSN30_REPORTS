8bc01028156c55a202abe4b021d00bec116c3ab6
Gillsbro <erik_gillsbro@hotmail.com>
2016-03-23 14:12:10 +0100

    Update project1.R

--- project1.R
+++ project1.R
@@ -1,5 +1,4 @@
-data <- scan("")
-
+#Read data from homepage
 data_1 <- read.table("http://www.maths.lth.se/matstat/kurser/masm22/exercise/dataCDI.txt",header=FALSE,sep="")
 colnames(data_1) <- c("id","county","state","area","pop","pop1834","pop65","phys","beds","crimes","higrads","bachelors","poors","unemployed","pci","totinc","region")
-data_1
+
